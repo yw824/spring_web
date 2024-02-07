9613a4a64a1e766d488660aefa6a4474acfcc848
yw824 <def47619>
2024-02-07 19:27:27 +0900

    <test> : Object Mapper : Test Code를 통해 Object Mapper의 기능 - 기능 1 : Object Mapper는 Mapping 할 때 Getter/Setter를 호출하여 매핑한다. 	- Object -> Text : Getter 사용 	- Text -> Object : Setter 사용 - 기능 2 : 디폴트 생성자가 있어야 한다. 	- 없으면 Object Mapper가 readValue(text->Object 매핑)할 때 에러 발생 	- http로 REST 요청할 때는 에러가 없지만, 직접 코드 내에서 생성한 json으로 object 생성 시 에러 	- 디폴트 생성자 만들어야 한다. - 에러 경우 3 : ObjectMapper가 사용할 DTO 객체의 임의의 사용자 작성 함수명 : get으로 시작하면 안된다. 	- serialize / deserialize할 때 에러 발생 	- Object Mapper는 객체 생성할 때 Getter를 한 번식 모두 호출한다. 	- 그런데, get으로 시작하는 함수를 Getter로 인식하여 getDefaultUser 함수 호출 	- 객체 생성 -> 디폴트객체 생성함수 호출 -> 디폴트객체 생성 -> 해당 객체위한 생성함수 다시호출 ,,, 	- 무한루프 발생 -> 에러 	- 해결책 : get으로 시작하지 않게끔 함수명 변경 OR static 함수로 변경

--- src/main/java/com/leew/hello/dto/UserRequest.java
+++ src/main/java/com/leew/hello/dto/UserRequest.java
@@ -1,9 +1,22 @@
 package com.leew.hello.dto;
 
 public class UserRequest {
+    public UserRequest(String name, String email, int age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    // Default Constructor
+    public UserRequest() {
+
+    }
+
     private String name;
     private String email;
     private int age;
+
+
 
     public String getName() {
         return name;
@@ -27,5 +40,18 @@ public class UserRequest {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    public static UserRequest getDefaultUser() {
+        return new UserRequest("Unknown", "noEmail@noemail.com", 0);
+    }
+
+    @Override
+    public String toString() {
+        return "UserRequest{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
--- src/test/java/com/leew/hello/HelloApplicationTests.java
+++ src/test/java/com/leew/hello/HelloApplicationTests.java
@@ -1,5 +1,8 @@
 package com.leew.hello;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.leew.hello.dto.UserRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +10,44 @@ import org.springframework.boot.test.context.SpringBootTest;
 class HelloApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws JsonProcessingException {
+        System.out.println("------Object Mapper------");
+
+        /*
+        Object Mapper :
+        Text JSON -> Object
+        Object -> Text JSON
+
+        JSON(text) -req-> Controller -> Map to Object
+        Object -> Controller -res-> Map to JSON(text)
+
+        직접 객체로 생성해서 활용할 수 있는 방법이 있다.
+         */
+
+        var objectMapper = new ObjectMapper();
+        // object -> text
+        var user = new UserRequest("scott", "scott@gmail.com", 20);
+
+        // JsonProcessingException 발생 가능 -> method에 throw Exception 처리해야 함
+        var text = objectMapper.writeValueAsString(user);
+        System.out.println(text); // 여기까지만 작성하면 에러 발생 -> DTO 가서 Getter/Setter 설정해야 함
+        // 특징 1 : Object Mapper는 Getter/Setter 사용한다.
+
+        // object -> text
+        var objectUser = objectMapper.readValue(text, UserRequest.class);
+        System.out.println(objectUser); // 에러 발생
+        /* 에러 로그 : 생성자가 없어 생성할 수 없다 : 근데 DTO 가면 생성자 있다.
+            Cannot construct instance of `com.leew.hello.dto.UserRequest`
+            (no Creators, like default constructor, exist):
+            cannot deserialize from Object value (no delegate- or property-based Creator)
+         */
+        // Default 생성자도 생성해야 한다. 생성 후에는 성공
+        // 특징 2 : Object Mapper는 default 생성자를 필요로 한다.
+
+        // 에러 발생하는 경우 3
+        UserRequest defaultUser = UserRequest.getDefaultUser();
+        // UserRequest defaultUser = UserRequest.defaultUser(); // get을 어두에서 없앤다.
+
     }
 
 }
