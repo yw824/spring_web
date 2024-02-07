ad21bb6f3ffe600df0debdefd628c0e00bc6f42f
yw824 <def47619>
2024-02-07 17:35:36 +0900

    <feat> : DELETE API Mapping - delete할 때는 key로 구별하여 데이터 삭제 -> Pathvariable 설정 	- delete/100 과 같은 형태로 요청 - Param 있는 경우에는 Get 형태와 같이 RequestParam 으로 요청 - delete 자체가 삭제 	- 있어서 삭제한 것도 200, 없는데 다시 삭제 요청한 것도 200 	- 굳이 '없어요" 말하지 않는다. 최종 상태가 그냥 없는 값이 된 상태를 완료한 상태로 간주

--- src/main/java/com/leew/hello/controller/DeleteApiController.java
+++ src/main/java/com/leew/hello/controller/DeleteApiController.java
@@ -1,15 +1,19 @@
 package com.leew.hello.controller;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class DeleteApiController {
 
-    @DeleteMapping("/delete")
-    public void delete() {
+    @DeleteMapping("/delete/{userId}")
+    public void delete(@PathVariable("userId") String userId, @RequestParam String account) {
+        // http://localhost:9090/api/delete/100?account=user100
+        System.out.println("userId: " + userId); // userId: 100
+        System.out.println("account: " + account); // account: user100
 
+        // delete 자체가 삭제 ->
+        // 있어서 삭제한 것도 200, 없는데 다시 삭제 요청한 것도 200
+        // 굳이 "없어요" 말할 필요는 없다. 그냥 없는 값이 된 상태가 완료된 상태로 간주
     }
 }
