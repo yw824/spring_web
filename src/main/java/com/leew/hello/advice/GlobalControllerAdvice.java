e5fbec1a69a8508a5d5df7f5c0789a449d923d57
yw824 <def47619>
2024-02-16 17:41:05 +0900

    <fix> : 가시성 있는 Response 제작 - ExceptionController : 	- 파라미터 필수 + 크기/값 범위 설정 - GlobalControllerAdvice : 	- HttpServletRequest 객체로, 내가 요청한 URI 정보를 가져옴 	- 요청의 결과를 먼저 BindingResult로 담아 가져온 후, 	- 에러들을 순회하며 에러를 가져온다. (FieldError 객체로) 	- 해당 FieldError 객체로 가져와서 errorMessage에 담은 후, ResponseEntity의 Body에 담아 반환 	- validation 조건에 맞지 않은 파라미터 값들을 가져오기 위해 Stream 객체 사용 		- error.getPropertyPath()로 파라미터 가져온 후 리스트로 저장 		- 각 리스트의 마지막 값의 이름 가져오면 param key값 - ErrorResponse : 에러 코드 ,결과값, 전달할 메세지, 요청 URL, 에러 리스트 등 담는 객체 - Error : 필드, 메세지, 필드에 들어간 부정확한 값

--- src/main/java/com/leew/hello/advice/GlobalControllerAdvice.java
+++ src/main/java/com/leew/hello/advice/GlobalControllerAdvice.java
@@ -1,13 +1,30 @@
 package com.leew.hello.advice;
 
+import com.leew.hello.controller.ValidationController;
+import com.leew.hello.dto.Error;
+import com.leew.hello.dto.ErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Path;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice() // 전체 Exception 다 잡을 예정
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+// 특정 Controller에만 적용되도록 basePackageClasses 설정
+@RestControllerAdvice(basePackageClasses = ValidationController.class) // 전체 Exception 다 잡을 예정
 // @ControllerAdvice // View-Resolver
 public class GlobalControllerAdvice {
 
@@ -23,9 +40,108 @@ public class GlobalControllerAdvice {
     }
 
     // 위에서 출력한 에러 내용을 바탕으로 해당 에러를 처리하는 함수 생성
+    // POST API의 값 없음 에러
     @ExceptionHandler(value= MethodArgumentNotValidException.class)
-    public ResponseEntity methodArgumentNotValidException(MethodArgumentNotValidException e) {
+    public ResponseEntity methodArgumentNotValidException(
+            MethodArgumentNotValidException e,
+            HttpServletRequest httpServletRequest // HTTP URI 요청 정보 갖고 있는 객체
+            ) {
         System.out.println("Exception Handler in Advice");
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+
+        List<Error> errorList = new ArrayList<>();
+
+        BindingResult bindingResult = e.getBindingResult();
+        bindingResult.getAllErrors().forEach(error -> {
+            FieldError field = (FieldError) error;
+
+            String fieldName = field.getField();
+            String message = field.getDefaultMessage();
+            String value = field.getRejectedValue().toString(); // 어떤 값이 잘못 들어갔는 지 확인
+
+            Error errorMessage = new Error();
+            errorMessage.setField(fieldName);
+            errorMessage.setMessage(message);
+            errorMessage.setInvalidValue(value);
+
+            errorList.add(errorMessage);
+        });
+
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setErrorList(errorList);
+        errorResponse.setMessage("");
+        errorResponse.setRequestUrl(httpServletRequest.getRequestURI());
+        errorResponse.setStatusCode(HttpStatus.BAD_REQUEST.toString());
+        errorResponse.setResultCode("FAIL");
+
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+    // GET API에서 값은 있는데, Valiation 조건에 맞지 않아 에러가 발생
+    @ExceptionHandler(value= ConstraintViolationException.class)
+    public ResponseEntity constraintViolationException(
+            ConstraintViolationException e,
+            HttpServletRequest httpServletRequest) { // HTTP URI 요청 정보 갖고 있는 객체
+        // 이 Exception : 어떠한 필드가 잘못되었는 지 정보를 담고 있다.
+        System.out.println("ConstraintViolationError");
+
+        List<Error> errorList = new ArrayList<>();
+
+        // 밑 문장에서 checkpoint 설정해 놓고 디버깅하면, 어떤 객체들이 있는지 알 수 있다.
+        e.getConstraintViolations().forEach(error -> {
+
+            // System.out.println(error.getPropertyPath()); // get.name, //get.age
+            // System.out.println(error.getPropertyPath().spliterator());
+
+            Stream<Path.Node> stream = StreamSupport.stream(error.getPropertyPath().spliterator(), false);
+            List<Path.Node> list = stream.collect(Collectors.toList());
+
+            String field = list.get(list.size() - 1).getName();
+            String message = error.getMessage();
+            String invalidMessage = error.getInvalidValue().toString();
+
+            Error errorMessage = new Error();
+            errorMessage.setField(field);
+            errorMessage.setMessage(message);
+            errorMessage.setInvalidValue(invalidMessage);
+
+            errorList.add(errorMessage);
+        });
+
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setErrorList(errorList);
+        errorResponse.setMessage("");
+        errorResponse.setRequestUrl(httpServletRequest.getRequestURI());
+        errorResponse.setStatusCode(HttpStatus.BAD_REQUEST.toString());
+        errorResponse.setResultCode("FAIL");
+
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+    // GET API의 값 없음 에러
+    @ExceptionHandler(value=MissingServletRequestParameterException.class)
+    public ResponseEntity missingServletRequestParameterException(
+            MissingServletRequestParameterException e,
+            HttpServletRequest httpServletRequest
+            ) {
+
+        List<Error> errorList = new ArrayList<>();
+
+        String fieldName = e.getParameterName();
+        String invalidValue = e.getMessage();
+
+        Error errorMessage = new Error();
+        errorMessage.setField(fieldName);
+        errorMessage.setMessage(e.getMessage());
+
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setErrorList(errorList);
+        errorResponse.setMessage("");
+        errorResponse.setRequestUrl(httpServletRequest.getRequestURI());
+        errorResponse.setStatusCode(HttpStatus.BAD_REQUEST.toString());
+        errorResponse.setResultCode("FAIL");
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 }
--- src/main/java/com/leew/hello/controller/ExceptionController.java
+++ src/main/java/com/leew/hello/controller/ExceptionController.java
@@ -2,19 +2,28 @@ package com.leew.hello.controller;
 
 import com.leew.hello.dto.ExceptionUser;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/exception")
+@Validated
 public class ExceptionController {
 
     @GetMapping("/get")
     public ExceptionUser get(
-            @RequestParam(required = false) String name, // 파라미터 없이도 동작 + 값은 null
-            @RequestParam(required = false) Integer age) {
+            @Size(min=2)
+            @RequestParam String name, // 이제 파라미터 없이는 동작하지 않는다.
+
+            @NotNull
+            @Min(1)
+            @RequestParam Integer age) {
         ExceptionUser user = new ExceptionUser();
         user.setName(name);
         user.setAge(age);
@@ -34,9 +43,11 @@ public class ExceptionController {
         return user;
     }
 
+    /*
     @ExceptionHandler(value= MethodArgumentNotValidException.class)
     public ResponseEntity methodArgumentNotValidException(MethodArgumentNotValidException e) {
         System.out.println("Exception Handler in Controller: ");
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
+     */
 }
