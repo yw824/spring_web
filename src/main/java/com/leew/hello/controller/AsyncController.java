ae51b2daf7a5a82156f164996a96e24419978591
yw824 <def47619>
2024-02-28 12:33:44 +0900

    <feat> : 리턴 값 있는 비동기 함수 작성 - Service : run 함수 추가 	- 비동기로 수행하며 리턴값을 반환하는 함수를 AsyncResult에 넣어서 반환한다. 	- 비동기 쓰레드에 들어갈 함수는 로직만 넣으면 된다. 이 함수도 비동기 설정할 필요X 	- 반환 자료형이 CompletableFuture이여야 함 - Controller에서도 CompletableFuture 자료형으로 반환해야 함

--- src/main/java/com/leew/hello/controller/AsyncController.java
+++ src/main/java/com/leew/hello/controller/AsyncController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.CompletableFuture;
+
 @RestController
 @RequestMapping("/async")
 @Slf4j
@@ -19,10 +21,9 @@ public class AsyncController {
     }
 
     @GetMapping("/")
-    public String hello() {
-        asyncService.hello();
-        log.info("Method END");
+    public CompletableFuture hello() {
+        log.info("Completable Future Init");
         // asyncService hello 함수가 동기/비동기일 때 Method END 메세지 실행 시점 비교
-        return "hello";
+        return asyncService.run();
     }
 }
--- src/main/java/com/leew/hello/service/AsyncService.java
+++ src/main/java/com/leew/hello/service/AsyncService.java
@@ -1,7 +1,9 @@
 package com.leew.hello.service;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.CompletableFuture;
@@ -9,8 +11,9 @@ import java.util.concurrent.CompletableFuture;
 @Service
 @Slf4j
 public class AsyncService {
-    @Async // 비동기로 동작하도록
-    public CompletableFuture hello() {
+    // @Async // 비동기로 동작하도록
+    // 비동기 방식을 수행하는데 비동식 annotation이 불필요하다,, ????
+    public String hello() { // 리턴값은 해당 자료형으로 전달 가능
         for(int i = 0; i < 10; ++i) {
             try {
                 Thread.sleep(2000); // 2초 동안 sleep
@@ -19,5 +22,11 @@ public class AsyncService {
                 e.printStackTrace();
             }
          }
+        return "Service: Hello";
+    }
+
+    @Async
+    public CompletableFuture run() {
+        return new AsyncResult(hello()).completable();
     }
 }
