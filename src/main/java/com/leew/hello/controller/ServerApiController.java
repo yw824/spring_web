52a41b845e38847f118f088d90b3c01093b9e0de
yw824 <def47619>
2024-03-02 11:47:51 +0900

    <feat> : 네이버 API 호출하는 Controller 설정 - 네이버 API + path + queryParam으로 일일이 설정 	- 모두 문자자열로 설정한 다음에 한 번에 encoding -> Uri - RequestEntity에 uri 담기 + header 설정 - RestTemplate에 RequestEntity 담아서 요청

--- src/main/java/com/leew/hello/controller/ServerApiController.java
+++ src/main/java/com/leew/hello/controller/ServerApiController.java
@@ -6,7 +6,15 @@ import com.leew.hello.dto.FilterDTO;
 import com.leew.hello.dto.Req;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @RestController
 @RequestMapping("/api/server")
@@ -54,6 +62,38 @@ public class ServerApiController {
         );
 
         return response; // 반환할 때도 Header + Body의 형태여야 하기 때문에 Req<FilterDTO>
+    }
+
+    @GetMapping("/naver")
+    public String naver() {
+        // https:// openapi.naver.com/v1/search/local.json?query=%EC%A3%BC%EC%8B%9D&display=10&start=1&sort=random
+
+        String query = "갈비집";
+
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://openapi.naver.com")
+                .path("/v1/search/local.json")
+                .queryParam("query", query)
+                .queryParam("display", 10)
+                .queryParam("start", 1)
+                .queryParam("sort", "random")
+                .encode().build().toUri();
+
+        // 주소부터 잘 만들어졌는지 부터 확인
+        log.info("uri: {}", uri);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        // header 등록
+        RequestEntity<Void> req = RequestEntity
+                .get(uri)
+                .header("X-Naver-Client-Id", "Your-Client-Id-Key")
+                .header("X-Naver-Client-Secret", "Your-Client-Secret-Key")
+                .build();
+
+        ResponseEntity<String> result = restTemplate.exchange(req, String.class);
+        return result.getBody(); // String to JSON 형태로 반환
+
     }
 
 }
